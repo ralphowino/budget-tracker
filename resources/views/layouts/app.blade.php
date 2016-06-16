49da064f625a2cf5b014530492f4cf07c288c252
Raphael Owino <raphael@ralphowino.com>
2016-06-16 09:09:25 +0300

    update readme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,14 +42,16 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                Laravel
+                Budget Tracker
             </a>
         </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                <li><a href="{{ url('/home') }}">Home</a></li>
+                <li><a href="{{ url('/accounts') }}">Accounts</a></li>
+                <li><a href="{{ url('/points') }}">Budget Points</a></li>
+                <li><a href="{{ url('/budgets') }}">Budgets</a></li>
             </ul>
 
             <!-- Right Side Of Navbar -->
